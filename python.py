8d116b196f5d9074a9b9c77e23752bad4fb30b26
harsh-33coder <harshs3023@gmail.com>
2026-06-15 16:16:34 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,7 +1,1 @@
-print("welcome!\nEnter the Dimension of rectangle\nTo get area")
-num1=float(input("Enter length of rectangle:"))
-num2=float(input("Enter breadth of rectangle:"))
-per=num1*num2+num1*num2
-print("The area of rectangle",num1*num2,"squnits")
-print("The peremeter of rectangle",per)
-
+print("congratulations")
