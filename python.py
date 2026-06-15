c395aa0ea02c824d91dc4447c3b649853e78f99c
harsh-33coder <harshs3023@gmail.com>
2026-06-15 10:56:13 +0000

    py7

--- python.py
+++ python.py
@@ -1,7 +1,2 @@
-print("welcome!\nEnter the Dimension of rectangle\nTo get area")
-num1=float(input("Enter length of rectangle:"))
-num2=float(input("Enter breadth of rectangle:"))
-per=num1*num2+num1*num2
-print("The area of rectangle",num1*num2,"squnits")
-print("The peremeter of rectangle",per)
-print("thank you")
+num=float(input("Enter the number:"))
+print("The square of a number",num*num,"\n The cube of number",num*num*num)
