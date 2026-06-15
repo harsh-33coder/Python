80399f39b24ae4b4db1a6b6e8551ff198c1ef0bd
harsh-33coder <harshs3023@gmail.com>
2026-06-15 10:39:53 +0000

    py

--- python.py
+++ python.py
@@ -1,4 +1,7 @@
-name1=str(input("Enter your first name:"))
-name2=str(input("Enter your second name:"))
-Name=name1+"!"+name2
-print(Name)
+print("welcome!\nEnter the Dimension of rectangle\nTo get area")
+num1=float(input("Enter length of rectangle:"))
+num2=float(input("Enter breadth of rectangle:"))
+per=num1*num2+num1*num2
+print("The area of rectangle",num1*num2,"squnits")
+print("The peremeter of rectangle",per)
+
