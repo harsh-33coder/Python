7b0a85535b5e8cc870c371f6bc194efc60be7cb2
Harsh Pal <harshs3023@gmail.com>
2026-06-16 09:18:10 +0530

    Python questions 10

--- Python learning.py
+++ Python learning.py
@@ -1,3 +1,3 @@
-num1=int(input("Enter 1 number:"))
-num2=int(input("Enter 2 number:"))
-print("Sum is:",num1+num2,"\nSubtracion is:",num1-num2,"\nmultiplication is ",num1*num2,"\nquatient is:",num1//num2)
+lth=int(input("Enter length of a rectangle:"))
+bth=int(input("Enter breadth of a rectangle:"))
+print("The area of rectangle:",lth*bth,"\nThe peremeter of rectangle:",(lth+bth)*2)
